83a9eb52ab9a13113876ebf3874c20b6770e4f2b
andreafancellu <andreafancellu123@gmail.com>
2023-07-19 16:15:17 +0200

    adding words things

--- processing_lexRes.py
+++ processing_lexRes.py
@@ -1,6 +1,8 @@
 import os
 from bson.json_util import dumps
 from path import *
+from collections import defaultdict
+
 '''
 Goal numero 1, per ogni risorsa:
 {id: nome risorsa,
@@ -62,8 +64,36 @@ def retrive_word(sentiment):
 			for line in f.readlines():
 				if '_' not in line:
 					#if line not in list_of_dict:
-					list_of_dict.append({'lemma': line, 'resources': [{'resource': file, 'sentiment': sentiment}]})
+					list_of_dict.append({'lemma': line, 'resources': [{'resource': file, 'sentiment': sentiment}]}) 
 	return list_of_dict
+
+def merge_dicts(list_of_dicts):
+	res_dict = defaultdict(list)
+	for dict in list_of_dicts:
+		if dict["lemma"] in res_dict["lemma"]:
+			res_dict["resources"].append(dict["resources"])
+		else:
+			res_dict.add({"lemma":dict["lemma"], "resources":dict["resources"]})
+	return res_dict
+
+
+'''def retrive_word(sentiment):
+	files = RESOURCES[sentiment]
+	list_of_dict = []
+	for file in files:
+		complete_path = os.path.join(BASE_PATH, f'{sentiment.capitalize()}/{file}_{sentiment}.txt')
+		with open(complete_path, 'r', encoding="utf8") as f: 
+			for line in f.readlines():
+				if '_' not in line:
+					if list_of_dict == []:
+						list_of_dict.append({'lemma': line, 'resources': [{'resource': file, 'sentiment': sentiment}]})
+					else:
+						for dict in list_of_dict:
+							if line != dict["lemma"]:
+								list_of_dict.append({'lemma': line, 'resources': [{'resource': file, 'sentiment': sentiment}]})
+							else:
+								dict["resources"].append({'resource': file, 'sentiment': sentiment})
+	return list_of_dict'''
 
 def main():
 	lex_res_word_list = []
@@ -78,12 +108,15 @@ def main():
 	#######
 	lex_res_word_list.clear()
 	for sentiment in RESOURCES.keys():
-		lex_res_word_list.extend(retrive_word(sentiment))
+		lex_res_word_list.extend(retrive_word(sentiment))	
+	list_final = merge_dicts(lex_res_word_list)
 	
+	print(list_final)
+
 	file = os.path.join(PREP_PATH, "LexResourcesWords.json")
 	with open(file, 'w', encoding='utf8') as f:
 		f.write(dumps(lex_res_word_list, indent=2))
-		print("Lexical Resources' Words json has been built correctly.")
+		print("Lexical Word Resources json has been built correctly.")
 
 
 
