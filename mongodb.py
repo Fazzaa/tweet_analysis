1212f0d3bc229b4bcbf71d65d1569f60bfedf28c
Fabio Luciani <lu.fa.98@gmail.com>
2023-07-19 12:49:18 +0200

    added credentials (sorry mama)

--- mongodb.py
+++ mongodb.py
@@ -1,1 +1,7 @@
-import pymongo
+from pymongo import MongoClient
+
+urlFabio = 'mongodb+srv://fabioluciani:WAn3fCWXuS54YG9R@sentimentanalysistwitte.kngytmy.mongodb.net/'
+urlAndrea = "mongodb+srv://andreafancellu:51yikZeu5I1nAMzR@sentimentanalysistwitte.kngytmy.mongodb.net/"
+
+client = MongoClient(urlFabio)
+print(client.list_database_names())
