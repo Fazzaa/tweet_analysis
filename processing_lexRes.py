6d674e5e14a040999dd6a0cb06fd2a02da21b537
Fabio Luciani <lu.fa.98@gmail.com>
2023-07-19 16:58:24 +0200

    completed extraction of lexical resources

--- processing_lexRes.py
+++ processing_lexRes.py
@@ -42,7 +42,7 @@ def retrive_information(sentiment):
 	files = RESOURCES[sentiment]
 	list_of_dict = []
 	for file in files:
-		complete_path = os.path.join(BASE_PATH, f'{sentiment.capitalize()}/{file}_{sentiment}.txt')
+		complete_path = os.path.join(PATH, f'{sentiment.capitalize()}/{file}_{sentiment}.txt')
 		with open(complete_path, 'r', encoding="utf8") as f: 
 			n_Words = 0
 			for line in f.readlines():
@@ -59,41 +59,25 @@ def retrive_word(sentiment):
 	files = RESOURCES[sentiment]
 	list_of_dict = []
 	for file in files:
-		complete_path = os.path.join(BASE_PATH, f'{sentiment.capitalize()}/{file}_{sentiment}.txt')
+		complete_path = os.path.join(PATH, f'{sentiment.capitalize()}/{file}_{sentiment}.txt')
 		with open(complete_path, 'r', encoding="utf8") as f: 
 			for line in f.readlines():
 				if '_' not in line:
-					#if line not in list_of_dict:
-					list_of_dict.append({'lemma': line, 'resources': [{'resource': file, 'sentiment': sentiment}]}) 
+					list_of_dict.append({'lemma': line.strip("\n"), 'resources': [{'resource': file, 'sentiment': sentiment}]}) 
 	return list_of_dict
 
 def merge_dicts(list_of_dicts):
-	res_dict = defaultdict(list)
-	for dict in list_of_dicts:
-		if dict["lemma"] in res_dict["lemma"]:
-			res_dict["resources"].append(dict["resources"])
-		else:
-			res_dict.add({"lemma":dict["lemma"], "resources":dict["resources"]})
+	res_dict = []
+	temp = []
+	for d1 in list_of_dicts:
+		if d1["lemma"] not in temp:
+			for d2 in list_of_dicts:
+				if (d1["lemma"] == d2["lemma"]) and d1["resources"] != d2["resources"]:
+					d1["resources"].extend(d2["resources"])
+			res_dict.append(d1)
+			temp.append(d1["lemma"])
 	return res_dict
 
-
-'''def retrive_word(sentiment):
-	files = RESOURCES[sentiment]
-	list_of_dict = []
-	for file in files:
-		complete_path = os.path.join(BASE_PATH, f'{sentiment.capitalize()}/{file}_{sentiment}.txt')
-		with open(complete_path, 'r', encoding="utf8") as f: 
-			for line in f.readlines():
-				if '_' not in line:
-					if list_of_dict == []:
-						list_of_dict.append({'lemma': line, 'resources': [{'resource': file, 'sentiment': sentiment}]})
-					else:
-						for dict in list_of_dict:
-							if line != dict["lemma"]:
-								list_of_dict.append({'lemma': line, 'resources': [{'resource': file, 'sentiment': sentiment}]})
-							else:
-								dict["resources"].append({'resource': file, 'sentiment': sentiment})
-	return list_of_dict'''
 
 def main():
 	lex_res_word_list = []
@@ -109,15 +93,14 @@ def main():
 	lex_res_word_list.clear()
 	for sentiment in RESOURCES.keys():
 		lex_res_word_list.extend(retrive_word(sentiment))	
+	print("Ciclo finito")
 	list_final = merge_dicts(lex_res_word_list)
 	
-	print(list_final)
 
 	file = os.path.join(PREP_PATH, "LexResourcesWords.json")
 	with open(file, 'w', encoding='utf8') as f:
-		f.write(dumps(lex_res_word_list, indent=2))
+		f.write(dumps(list_final, indent=2))
 		print("Lexical Word Resources json has been built correctly.")
-
 
 
 if __name__== '__main__':
