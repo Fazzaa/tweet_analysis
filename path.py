21ed853bef12cdcfb0956da7e9abb98089858232
Fabio Luciani <lu.fa.98@gmail.com>
2023-07-20 12:23:23 +0200

    added path

--- path.py
+++ path.py
@@ -1,12 +1,6 @@
 MASK_PATH = "/home/fazza/Desktop/PINKLADYFINALE.png"
 NEG_WORDS_PATH = "/home/fazza/Desktop/materiale_maadb/risorse_lessicali/elenco-parole-che-negano-parole-successive.txt"
 PATH = "/home/fazza/Desktop/materiale_maadb/risorse_lessicali"
-PREP_PATH = "/home/fazza/Desktop/materiale_maadb/preprocessed_lex_res"
-TWEET_PATH = r"/home/fazza/Desktop/materiale_maadb/messaggi_twitter\dataset_dt_{}_60k.txt"
+PREP_PATH = "/home/fazza/Desktop/materiale_maadb/preprocessed_res"
+TWEET_PATH = "/home/fazza/Desktop/materiale_maadb/messaggi_twitter/dataset_dt_{}_60k.txt"
 
-'''MASK_PATH = r"C:\Users\andre\Desktop\PINKLADYFINALE.png"
-NEG_WORDS_PATH = r"C:\Users\andre\Desktop\Progetti\ProgettoMAADBmateriale\Risorse_lessicali\Archive_risorse_lessicali\elenco-parole-che-negano-parole-successive.txt"
-PATH = r"C:\Users\andre\Desktop\Progetti\ProgettoMAADBmateriale\Risorse_lessicali\Archive_risorse_lessicali"
-PREP_PATH = r"C:\Users\andre\Desktop\Progetti\ProgettoMAADBmateriale\preprocessed_lex_res"
-TWEET_PATH = r"C:\Users\andre\Desktop\Progetti\ProgettoMAADBmateriale\Twitter_messaggi\dataset_dt_{}_60k.txt"
-'''
